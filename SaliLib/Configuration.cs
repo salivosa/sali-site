cbdef06a3b162958576e66c5e3bdc529ccdc83ec
sali <Salvadore0ran@gmail.com>
2020-09-08 02:21:21 -0400

    realized chromeDriver sucks when on web hosting so i removed it. wil use TwitterOps for some tweaks

--- SaliLib/Configuration.cs
+++ SaliLib/Configuration.cs
@@ -1,50 +1,34 @@
 ﻿using HtmlAgilityPack;
-using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
+using System.Configuration;
+using TwitterOps;
 
 namespace SaliLib
 {
     public class Configuration
     {
-        //get Twitter Data
-        public static Dictionary<string, object> get_Twitter_data(string id)
+        private static Operations ops { get; set; }
+        public Configuration(string consumerKey, string consumerSecret, string tokenValue, string tokenSecret)
         {
-            // find chromedrive exe in binaries
-            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            ops = new Operations(consumerKey, consumerSecret, tokenValue, tokenSecret);
+        }
 
-            //open driver headless
-            var options = new ChromeOptions();
-            options.AddArgument("headless");
-            var driver = new ChromeDriver(path, options);
+        //get Twitter Data
+        public Dictionary<string, object> get_Twitter_data(string id)
+        {
 
-            //navigate to page
-            driver.Navigate().GoToUrl("https://twitter.com/intent/user?user_id=" + id);
-
-            Thread.Sleep(10 * 1000);
-
-            //get html data from loaded page
-            var html_data = driver.PageSource;
-
-            //quit page
-            driver.Quit();
-
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(html_data);
-
-            var url = "https://twitter.com/intent/user?user_id=" + id;
-            var node = doc.DocumentNode;
+            var user = ops.Users.GetUserById(id);
 
             var dict = new Dictionary<string, object>()
             {
-                { "user_title", node.SelectNodes("//title").Select(x => x.InnerText.Split(" (")[0].Trim()).FirstOrDefault() },
-                { "twitter_page", "https://twitter.com/" +  node.SelectNodes("//title").Select(x => x.InnerText.Split("(")[1].Replace("@", "").Trim()).FirstOrDefault() },
-                { "user_photo", node.SelectNodes("//meta[@property='og:image']").Select(x => x.GetAttributeValue("content", "").Replace("normal", "200x200")).FirstOrDefault() },
-                { "user_id",  id}
+                { "user_title", user.username },
+                { "twitter_page", "https://twitter.com/" +  user.username },
+                { "user_photo", user.profile_image_url }
             };
 
             return dict;
--- sali-site/Controllers/HomeController.cs
+++ sali-site/Controllers/HomeController.cs
@@ -17,35 +17,38 @@ namespace sali_site.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration _configuration;
+        private static Configuration config { get; set; }
 
         public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
 
-            if (twitter_data == null)
+            if (config == null)
             {
+                config = new Configuration(_configuration.GetSection("consumerKey").Get<string>(), _configuration.GetSection("consumerSecret").Get<string>(), _configuration.GetSection("tokenValue").Get<string>(), _configuration.GetSection("tokenSecret").Get<string>());
+
+                //Useful timer i might use later
+
                 //Timer for update variable every 60 minutes
-                var aTimer = new Timer(60 * 60 * 1000);
-                aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-                aTimer.Start();
+                //var aTimer = new Timer(60 * 60 * 1000);
+                //aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+                //aTimer.Start();
             }
         }
 
         private static Dictionary<string, object> twitter_data { get; set; }
 
-        //Every hour, update variable twitter_data
+        //Task which will be handly in a future
         private static void OnTimedEvent(object source, ElapsedEventArgs e)
         {
-            twitter_data = Configuration.get_Twitter_data(twitter_data["user_id"].ToString());
         }
 
         public IActionResult Index()
         {
             var TwitterUserId = _configuration.GetSection("Twitter ID").Get<string>();
 
-            if (twitter_data == null)
-                twitter_data = Configuration.get_Twitter_data(TwitterUserId);
+            var twitter_data = config.get_Twitter_data(TwitterUserId);
 
             ViewData["user_title"] = twitter_data["user_title"];
             ViewData["user_photo"] = twitter_data["user_photo"];
@@ -62,7 +65,7 @@ namespace sali_site.Controllers
         [HttpGet]
         public void RefreshTwitterData()
         {
-            twitter_data = Configuration.get_Twitter_data(twitter_data["user_id"].ToString());
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
